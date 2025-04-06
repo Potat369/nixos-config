1e09d551f553deab1c858dcd2349e54e53f14eac
Potat369 <yevheniidemian@gmail.com>
2025-04-06 21:51:51 +0300

    Setup hyprland

--- configuration.nix
+++ configuration.nix
@@ -80,6 +80,10 @@
   programs.fish = {
     enable = true;
   };
+  programs.hyprland = {
+    withUWSM = true;
+    enable = true;
+  };
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
