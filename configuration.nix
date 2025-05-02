e5478441d9e184a5571c881ae354d5e58a4a8467
Potat369 <yevheniidemian@gmail.com>
2025-05-02 12:26:52 +0300

    nvidia

--- configuration.nix
+++ configuration.nix
@@ -80,6 +80,11 @@
   hardware.nvidia = {
     open = true;
     prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:2:0:0";
     };
